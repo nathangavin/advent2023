4a0d109d7fc3436f976fa0043a148a096903b395
Nathan Gavin <nathangavin987@gmail.com>
2023-12-04 13:23:04 -0700

    day 3 part 2

--- day3/src/index.ts
+++ day3/src/index.ts
@@ -5,12 +5,16 @@ const file = readFileSync('input.txt', 'utf-8');
 
 const lines = file.split('\n');
 
-// find number in row
-// look around number to see if there are symbols
+let partNumberPositions : Array<Array<[number,number]>> = [];
+let ratioSum = 0;
 
-let totalPartNumbers = 0;
+for (const line of lines) {
+    if (line.trim().length == 0) continue;
+    partNumberPositions.push([]);
+}
 
 for (let i = 0; i < lines.length; i++) {
+    
     let line = lines[i];
     if (line.trim().length == 0) continue;
 
@@ -24,46 +28,92 @@ for (let i = 0; i < lines.length; i++) {
             lastPos = j - 1;
             j--;
             
+            let rowFound = -1;
             let validPart = false;
             for (let x = firstPos - 1; x <= lastPos + 1; x++) {
                 if (x < 0) continue;
                 if (x >= line.length) break;
-                if (i > 0 && isCharSymbol(lines[i - 1][x])) {
-                    console.log(`Above: row: ${i - 1}, col: ${x}, val: ${lines[i - 1][x]}`);
+                if (i > 0 && isCharStar(lines[i - 1][x])) {
+                    rowFound = i;
                     validPart = true;
                     break;
-                } else if (i < lines.length - 2 && isCharSymbol(lines[i + 1][x])) {
-                    console.log(`Below: row: ${i + 1}, col: ${x}, val: ${lines[i + 1][x]}`);
+                } else if (i < lines.length - 2 && isCharStar(lines[i + 1][x])) {
+                    rowFound = i;
                     validPart = true;
                     break;
                 }
             }
 
             if (!validPart) {
-                if (firstPos > 0 && isCharSymbol(line[firstPos - 1])) {
-                    console.log(`Left: row: ${i}, col: ${firstPos - 1}, val: ${line[firstPos - 1]}`);
+                if (firstPos > 0 && isCharStar(line[firstPos - 1])) {
+                    rowFound = i;
                     validPart = true;
-                } else if (lastPos < line.length - 1 && isCharSymbol(line[lastPos + 1])) {
-                    console.log(`Right: row: ${i}, col: ${lastPos + 1}, val: ${line[lastPos + 1]}`);
+                } else if (lastPos < line.length - 1 && isCharStar(line[lastPos + 1])) {
+                    rowFound = i;
                     validPart = true;
                 }
             }
 
             if (validPart) {
-                let partNumberStr = "";
-                for (let k = firstPos; k <= lastPos; k++) {
-                    partNumberStr += line[k];
-                }
-                let partNumber = Number.parseInt(partNumberStr);
-                if (!isNaN(partNumber)) {
-                    totalPartNumbers += partNumber;
-                }
+                console.log(`row: ${i}, first: ${firstPos}, last: ${lastPos}`);
+                partNumberPositions[rowFound].push([firstPos,lastPos]);
             }
         }
     }
 }
 
-console.log(totalPartNumbers);
+for (let i = 0; i < lines.length; i++) {
+    let line = lines[i];
+    if (line.trim().length == 0) continue;
+
+    for (let j = 0; j < line.length; j++) {
+        let char = line[j];
+
+        if (isCharStar(char)) {
+            let adjacentNumberPositions : Array<[number, number, number]> = [];
+            if (i > 0 && i < lines.length - 2) {
+                for (let row = i - 1; row <= i + 1; row++) {
+                    for (const pair of partNumberPositions[row]) { 
+                        let isNear = false;
+                        for (let col = j - 1; col <= j + 1; col++) {
+                            if (pair[0] <= col && pair[1] >= col) {
+                                isNear = true;
+                                break;
+                            }
+                        }
+                        if (isNear) {
+                            adjacentNumberPositions.push([row, pair[0], pair[1]]);
+                        }
+                    }
+                }
+            }
+
+            if (adjacentNumberPositions.length == 2) {
+                let ratio = 1;
+                
+                for (const pos of adjacentNumberPositions) {
+                    let partNumberStr = "";
+                    let [row, firstPos, lastPos] = pos;
+                    if (typeof row == 'number' && !isNaN(row) &&
+                        typeof firstPos == 'number' && !isNaN(firstPos) &&
+                        typeof lastPos == 'number' && !isNaN(lastPos)) {
+                            for (let k = firstPos; k <= lastPos; k++) {
+                                partNumberStr += lines[pos[0]][k];
+                            }
+                            let partNumber = Number.parseInt(partNumberStr);
+                            if (!isNaN(partNumber)) {
+                                ratio *= partNumber;
+                            }
+                    }
+                }
+
+                ratioSum += ratio;
+            }
+        }
+    }
+}
+
+console.log(ratioSum);
 
 function isCharNumeric(c: string) : boolean {
     return /\d/.test(c);
@@ -71,4 +121,8 @@ function isCharNumeric(c: string) : boolean {
 
 function isCharSymbol(c: string) : boolean {
     return /[^\d.]/.test(c);
+}
+
+function isCharStar(c: string) : boolean {
+    return /[*]/.test(c);
 }
---
+++ day3/src/part1.ts
@@ -0,0 +1,75 @@
+
+import { readFileSync } from 'fs';
+
+const file = readFileSync('input.txt', 'utf-8');
+//const file = readFileSync('test.txt', 'utf-8');
+
+const lines = file.split('\n');
+
+// find number in row
+// look around number to see if there are symbols
+
+let totalPartNumbers = 0;
+
+for (let i = 0; i < lines.length; i++) {
+    let line = lines[i];
+    if (line.trim().length == 0) continue;
+
+    let firstPos = -1;
+    let lastPos = -1;
+    for (let j = 0; j < line.length; j++) {
+        let char = line[j];
+        if (isCharNumeric(char)) {
+            firstPos = j;
+            while (isCharNumeric(line[j])) j++;
+            lastPos = j - 1;
+            j--;
+            
+            let validPart = false;
+            for (let x = firstPos - 1; x <= lastPos + 1; x++) {
+                if (x < 0) continue;
+                if (x >= line.length) break;
+                if (i > 0 && isCharSymbol(lines[i - 1][x])) {
+                    console.log(`Above: row: ${i - 1}, col: ${x}, val: ${lines[i - 1][x]}`);
+                    validPart = true;
+                    break;
+                } else if (i < lines.length - 2 && isCharSymbol(lines[i + 1][x])) {
+                    console.log(`Below: row: ${i + 1}, col: ${x}, val: ${lines[i + 1][x]}`);
+                    validPart = true;
+                    break;
+                }
+            }
+
+            if (!validPart) {
+                if (firstPos > 0 && isCharSymbol(line[firstPos - 1])) {
+                    console.log(`Left: row: ${i}, col: ${firstPos - 1}, val: ${line[firstPos - 1]}`);
+                    validPart = true;
+                } else if (lastPos < line.length - 1 && isCharSymbol(line[lastPos + 1])) {
+                    console.log(`Right: row: ${i}, col: ${lastPos + 1}, val: ${line[lastPos + 1]}`);
+                    validPart = true;
+                }
+            }
+
+            if (validPart) {
+                let partNumberStr = "";
+                for (let k = firstPos; k <= lastPos; k++) {
+                    partNumberStr += line[k];
+                }
+                let partNumber = Number.parseInt(partNumberStr);
+                if (!isNaN(partNumber)) {
+                    totalPartNumbers += partNumber;
+                }
+            }
+        }
+    }
+}
+
+console.log(totalPartNumbers);
+
+function isCharNumeric(c: string) : boolean {
+    return /\d/.test(c);
+}
+
+function isCharSymbol(c: string) : boolean {
+    return /[^\d.]/.test(c);
+}
