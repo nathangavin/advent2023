bb027a58072c9875a8b9d45b112f046e523cbd0e
Nathan Gavin <nathangavin987@gmail.com>
2023-12-04 16:25:25 -0700

    day 4 part 2

--- day4/src/index.ts
+++ day4/src/index.ts
@@ -6,7 +6,8 @@ const file = readFileSync('input.txt', 'utf-8');
 
 const lines = file.split('\n');
 
-let totalPoints = 0;
+let cardCopies = new Map<number, number>();
+
 for (const line of lines) {
     if (line.trim().length == 0) continue;
     let matches = 0;
@@ -14,9 +15,18 @@ for (const line of lines) {
     let winningSet = new Set<number>();
 
     let [first, second] = line.split('|');
-    let printLine = first + ' | ';
-    let [_, winningNumsStr] = first.split(':');
-
+    let [cardInfo, winningNumsStr] = first.split(':');
+    
+    let [_, cardIdStr] = cardInfo
+                            .trim()
+                            .replace('   ', ' ')
+                            .replace('  ', ' ')
+                            .split(' ');
+    let cardId = Number.parseInt(cardIdStr);
+    if (typeof cardId == 'number' && !isNaN(cardId) && !cardCopies.has(cardId)) {
+        cardCopies.set(cardId, 1);
+    } 
+    
     let winningNums = winningNumsStr.trim().split(' ');
 
     for (const numStr of winningNums) {
@@ -31,19 +41,32 @@ for (const line of lines) {
         let candNum = Number.parseInt(candidate);
         if (typeof candNum == 'number' && !isNaN(candNum)) {
             if (winningSet.has(candNum)){
-                printLine += 'T ';
                 matches++;
-            } else {
-                printLine += 'F ';
-            }
+            } 
         }
     }
 
-    printLine += ' - ' + candidates.length;
-
     if (matches > 0) {
-        totalPoints += 2 ** (matches - 1);
+        let numCopies = cardCopies.get(cardId);
+        if (typeof numCopies == 'number' && !isNaN(numCopies)) {
+            for (let i = cardId + 1; i <= cardId + matches; i++) {
+                if (!cardCopies.has(i)) {
+                    cardCopies.set(i, 1 + numCopies);
+                } else {
+                    let cardAmount = cardCopies.get(i);
+                    if (typeof cardAmount == 'number' && !isNaN(cardAmount)) {
+                        cardCopies.set(i, cardAmount + numCopies);
+                    } 
+                }
+            }
+        } 
     }
 }
 
-console.log(totalPoints);
+let numCards = 0;
+for (const [_, value] of cardCopies) {
+    numCards += value;
+}
+
+console.log(numCards);
+
