3edf929695498605bb3eb533cf1554d7e516cb8e
Nathan Gavin <nathangavin987@gmail.com>
2023-12-06 11:44:28 -0700

    day 6 part 2

--- day6/src/index.ts
+++ day6/src/index.ts
@@ -10,38 +10,31 @@ let [timeLine, distanceLine] = [lines[0], lines[1]];
 let [timeName, timeNumbersStr] = timeLine.split(':');
 let [distanceName, distanceNumbersStr] = distanceLine.split(':');
 
-let timeNumbers = timeNumbersStr
-                            .trim()
-                            .split(/[ ]+/)
-                            .map(s => {
-                                return Number.parseInt(s);
-                            });
+let time = Number.parseInt(timeNumbersStr.replace(/[ ]+/gi, ''));
+    
+let distance = Number.parseInt(distanceNumbersStr.replace(/[ ]+/gi, ''));
 
-let distanceNumbers = distanceNumbersStr
-                                    .trim()
-                                    .split(/[ ]+/)
-                                    .map(s => {
-                                        return Number.parseInt(s);
-                                    });
 
-let waysToWin = [];
-for (let i = 0; i < timeNumbers.length; i++) {
-    let numberOfWins = 0;
+let minTime = 0;
 
-    let time = timeNumbers[i];
-    let dist = distanceNumbers[i];
-    for (let timeHeld = time - 1; timeHeld > 0; timeHeld--) {
-        let distanceTravelled = (time - timeHeld) * timeHeld;
-        if (distanceTravelled > dist) {
-            numberOfWins++;
-        }
+for (let timeHeld = 1; timeHeld < time; timeHeld++) {
+    let distanceTravelled = (time - timeHeld) * timeHeld;
+    if (distanceTravelled > distance) {
+        minTime = timeHeld;
+        break;
     }
-    waysToWin.push(numberOfWins);
 }
 
-let total = 1;
-for (const num of waysToWin) {
-    total *= num;
-} 
+let maxTime = time;
 
-console.log(total);
+for (let timeHeld = time - 1; timeHeld > 0; timeHeld--) {
+    let distanceTravelled = (time - timeHeld) * timeHeld;
+    if (distanceTravelled > distance) {
+        maxTime = timeHeld;
+        break;
+    }
+}
+
+console.log(minTime);
+console.log(maxTime);
+console.log(maxTime - minTime + 1);
